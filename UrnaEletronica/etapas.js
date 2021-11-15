ec69ac4260242fa428c358569763652b48fe0fd0
abnerpsantos <abner.saan09@gmail.com>
2021-11-15 12:05:20 -0300

    Eletronic ballot box finished
    A Brazillian eletronic ballot box with HTML,CSS and JS.

---
+++ UrnaEletronica/etapas.js
@@ -0,0 +1,61 @@
+let etapas = [{
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [{
+                numero: '12345',
+                nome: 'Alice',
+                partido: 'Trevas',
+                fotos: [{
+                    url: 'splash_alice.jpg',
+                    legenda: 'Vereador'
+                }]
+            },
+            {
+                numero: '54321',
+                nome: 'Esmeralda',
+                partido: 'Monyan',
+                fotos: [{
+                    url: 'splash_esmeralda.jpg',
+                    legenda: 'Vereador'
+                }]
+            },
+        ]
+    },
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos: [{
+                numero: '12',
+                nome: 'Alucard',
+                partido: 'Trevas',
+                vice: 'Estes',
+                fotos: [{
+                        url: 'splash_alucard.jpg',
+                        legenda: 'Prefeito'
+                    },
+                    {
+                        url: 'splash_estes.jpg',
+                        legenda: 'Vice-Prefeito',
+                        small: true
+                    }
+                ]
+            },
+            {
+                numero: '21',
+                nome: 'Hayabusa',
+                partido: 'Monyan',
+                vice: 'Angela',
+                fotos: [{
+                        url: 'splash_hayabusa.jpg',
+                        legenda: 'Prefeito'
+                    },
+                    {
+                        url: 'splash_angela.jpg',
+                        legenda: 'Vice-Prefeito',
+                        small: true
+                    }
+                ]
+            }
+        ]
+    }
+]
