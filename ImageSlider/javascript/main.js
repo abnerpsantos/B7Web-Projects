f85cf68e05175fce6bdf6a4d816e16d252b6b2df
abnerpsantos <abner.saan09@gmail.com>
2021-11-15 12:01:07 -0300

    Slider Finished
    A slider for images with HTML,CSS and Javascript

---
+++ ImageSlider/javascript/main.js
@@ -0,0 +1,31 @@
+let totalSlides = document.querySelectorAll('.slider_item').length;
+let currentSlide = 0;
+
+
+document.querySelector('.slider_width').style.width = `calc(100vw * ${totalSlides})`;
+
+function prevBtn() {
+    currentSlide--;
+
+    if(currentSlide < 0) {
+        currentSlide = totalSlides - 1;
+    }
+    updateMargin();
+}
+
+function nextBtn() {
+    currentSlide++;
+
+    if(currentSlide > (totalSlides - 1)){
+        currentSlide = 0;
+    }
+    updateMargin();
+}
+
+function updateMargin() {
+    let itemWidth = document.querySelector('.slider_item').clientWidth;
+    let newMargin = (currentSlide * itemWidth);
+    document.querySelector('.slider_width').style.marginLeft = `-${newMargin}px`;
+}
+
+setInterval(nextBtn, 5000);
